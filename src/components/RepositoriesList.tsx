103dff9b5a849071bb6624f636d3297c0c34bde4
ismail harmanda <iharmanda@hotmail.com>
2021-11-26 01:36:29 +0300

    selecting state

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState<string>("");
 
   const { SearchRepositories } = useActions();
+  const { data, error, loading } = useSelector(
+    (state: any) => state.repositories
+  );
 
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setTerm(e.target.value);
