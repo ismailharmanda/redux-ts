0037f55ee9c4568775d78d949fd3420331a7d690
ismail harmanda <iharmanda@hotmail.com>
2021-11-26 00:10:42 +0300

    handle onChange and onSubmit events with TS

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,20 @@
+import React, { ChangeEventHandler, useState } from "react";
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState<string>("");
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setTerm(e.target.value);
+  };
+
+  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={onFormSubmit}>
+        <input value={term} onChange={onInputChange} type="text" />
         <button>Search</button>
       </form>
     </div>
